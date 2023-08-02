d20a0ba2bcd5aa5c991ca694a16089250c5c73e9
aura-gv <gauravkhatiwada4@gmail.com>
2023-08-02 15:33:47 +0545

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common-components/header/header.component';
+import { CartPageComponent } from './common-components/cart-page/cart-page.component';
+import { ProductsComponent } from './common-components/products/products.component';
+import { ServiceComponent } from './common-service/service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartPageComponent,
+    ProductsComponent,
+    ServiceComponent,
   ],
   imports: [
     BrowserModule,
