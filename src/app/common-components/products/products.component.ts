85f01d7aa8e10747518097f02535c403e6168fc6
aura-gv <gauravkhatiwada4@gmail.com>
2023-08-03 22:14:43 +0545

    second header and items from api

--- src/app/common-components/products/products.component.ts
+++ src/app/common-components/products/products.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonServiceService } from 'src/app/services/common-service.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  public productList: any;
+  constructor(private service: CommonServiceService) {}
 
+  ngOnInit(): void {
+    this.service.getProduct().subscribe((res) => {
+      this.productList = res;
+    });
+  }
 }
--- src/app/services/common-service.service.ts
+++ src/app/services/common-service.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommonServiceService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getProduct() {
+    return this.http.get<any>('https://fakestoreapi.com/products/').pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
 }
